6df9c9acbc78dd9a1fbd5b6cd28cd230cf03d692
Dwai07 <66767869+Dwai07@users.noreply.github.com>
2020-06-16 03:27:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/AizsP24En4");
+  ifrm.setAttribute("src", "DSC00018.JPG");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
